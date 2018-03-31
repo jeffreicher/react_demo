eac1b1b39553830b5fa53c54425c1b9661e6551d
Jeffrei Cher <jeffreicher@yahoo.com>
2018-03-31 16:45:19 -0700

    Changed header h3 to h4

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
 export default (props) => (
-    <div className="center card"><h3 className="card-content">{props.title}</h3></div>    
+    <div className="center card"><h4 className="card-content">{props.title}</h4></div>    
 );
 
