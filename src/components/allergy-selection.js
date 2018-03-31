4460f77a080798cee8bd4d2a6d6d70bf1793e54b
Jeffrei Cher <jeffreicher@yahoo.com>
2018-03-31 16:43:48 -0700

    Added card-content to header

---
+++ src/components/allergy-selection.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class AllergySelection extends Component {
+    render() {
+        return (
+            <div>
+                <p>Hello</p>
+            </div>
+        );
+    };
+};
+
+export default AllergySelection;
--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
 export default (props) => (
-    <div className="center"><h3>{props.title}</h3></div>    
+    <div className="center card"><h3 className="card-content">{props.title}</h3></div>    
 );
 
