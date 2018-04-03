1047f31d6b1941e4cef3f1b0a45a4479075f9543
Jeffrei Cher <jeffreicher@yahoo.com>
2018-04-02 19:04:27 -0700

    Added meal number section

--- src/components/app.js
+++ src/components/app.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import DietSelection from './diet-selection';
 import AllergySelection from './allergy-selection';
+import MealNumber from './meal_number';
 import 'materialize-css/dist/css/materialize.min.css';
 import '../assets/css/diet-selection.css';
 
@@ -11,5 +12,6 @@ export default () => (
         <Route exact path="/" />
         <Route path="/diet-selection" component={DietSelection} />
         <Route path="/allergy-selection" component={AllergySelection} />
+        <Route path="/mealnumber" component={MealNumber} />
     </div>
 );
---
+++ src/components/meal_number.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import Header from './header';
+import Button from './button';
+import Next from './next_button';
+import '../assets/css/mealNumber.css';
+
+class MealNumber extends Component {
+    render() {
+        return (
+            <div className="container">
+                <Header title={'Amount Of Meals?'} />
+                <div className="button-column">
+                    <Button title={'1'} style={'grey'} />  
+                    <Button title={'3'} style={'grey'} />
+                    <Button title={'5'} style={'grey'} />   
+                    <Button title={'7'} style={'grey'} />   
+                </div>  
+                <div className="right bottom">
+                <Next style={'bottom'}/>                  
+                </div>  
+            </div>
+        );
+    };
+};
+
+export default MealNumber;
