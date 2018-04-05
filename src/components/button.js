d76b18db331073386d573077af91eed1706b34f8
Jeffrei Cher <jeffreicher@yahoo.com>
2018-04-05 00:49:07 -0700

    Moved around text so that it looks better as well as started to add functionality for buttons

--- src/components/button.js
+++ src/components/button.js
@@ -1,11 +1,51 @@
 //description does not show up on button
-import React from 'react';
+import React, { Component } from 'react';
 
-export default props => (
-    <div>
-        <a className={`btn-desk waves-effect waves-light btn ${props.style}`}>
-            {props.title} <br/>
-            {props.descrip}
-        </a>
-    </div>
-);
+class Button extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            clicked: true
+        }
+    }
+
+    active() {
+
+    }
+
+    handleClick() {
+        const { clicked } = this.state;
+        if(clicked) {
+            console.log('true');
+        };
+        this.setState({
+            clicked: !this.state.clicked
+        });
+    };
+
+    render() {
+        // console.log(this.state.clicked);
+        // const active = this.state.clicked;
+        // if(active) {
+
+        // }
+        return (
+            <div>
+                <a onClick={this.handleClick.bind(this)} className={`btn-desk waves-effect waves-light btn ${this.props.style}`}>
+                <div>
+                    <p>
+                       {this.props.title} 
+                    </p>
+                     <br/>
+                     <p>
+                       {this.props.descrip}   
+                    </p>                                       
+                </div>
+                </a>
+            </div>
+        );
+    };
+};
+
+export default Button;
--- src/components/diet-selection.js
+++ src/components/diet-selection.js
@@ -6,33 +6,13 @@ import '../assets/css/diet-selection.css';
 
 //add method that adds class to add button color when clicked 
 
-class DietSelection extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            isToggleOn: 2
-        };
-
-        this.handleButtonClick = this.handleButtonClick.bind(this);
-    };
-
-    handleButtonClick() { //send data to state and keep active color
-        console.log(this.state.isToggleOn);
-        this.setState(event => ({
-            isToggleOn: !event.isToggleOn
-        }));
-        console.log(event);
-        
-
-    };  
-    
+class DietSelection extends Component {    
     render() {
         return (
         <div className="container">
             <Header title={'What\'s Your Type?'}/>
             <div className="collection center-align">
-                <Button onClick={this.handleButtonClick} title={'None'} descrip={'Ain\'t Scared Of Shit'} style={'button'} />  
+                <Button title={'None'} descrip={'Ain\'t Scared Of Shit'} style={'button'} />  
                 <Button title={'Ketogenic'} descrip={'Coachella Bod Meal Plan'} style={'button'} />     
                 <Button title={'Vegetarian'} descrip={'Straight Veggies N\' A Side Of Eggs'} style={'button'} />   
                 <Button title={'Vegan'} descrip={'Literally Straight Veggies'} style={'button'} />   
